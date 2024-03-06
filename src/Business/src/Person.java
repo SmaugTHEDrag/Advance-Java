6c4155993df33670d37bafaa9820070162399e84
LAPTOP-5SG2CNP0\NGUYEN <thainguyen122004@gmail.com>
2024-03-06 14:22:43 +0700

    add variable for Person

--- src/Business/src/Person.java
+++ src/Business/src/Person.java
@@ -1,3 +1,19 @@
-public class Person {
+import business.Company;
 
+public class Person {
+    private final double PI = 3.14;
+    private final String constant = "Hello";
+    private String firstName = "Nguyen";
+    private int age;
+    private long id;
+    private char MiddleInit;
+    private byte MyByte;
+    private static Company company = new Company();
+    private static Company company1 = new Company();
+    private NewsAgency agency;
+
+    public static void main(String[] args) {
+        System.out.println(company);
+        System.out.println(company1);
+    }
 }
